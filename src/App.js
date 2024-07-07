8d339ae9c1d75be544fd498c021a394470194502
unknown <robpoquin@gmail.com>
2024-07-07 21:00:08 +0200

    Creation fichier app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='sidebar'>
+        <div className='upperSide'>
+
+        </div>
+        <div className='lowerSide'>
+
+        </div>
+      </div>
+      <div className='main'>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;

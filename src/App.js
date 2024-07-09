c181335ebf52e2656dcad355c75027146c563c59
unknown <robpoquin@gmail.com>
2024-07-09 20:31:48 +0200

    Creation SideBar partie haute et ajout des styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
+import gptLogo from './assets/chatgpt.svg';
 
 function App() {
   return (
     <div className="App">
-      <div className='sidebar'>
+      <div className='sideBar'>
+{/*---------- upperSide -------------*/}
         <div className='upperSide'>
+    {/*---------- upperSideTop -------------*/}
+          <div className='upperSideTop'>
+            <img src={gptLogo} alt='Logo' className='logo'/>
+            <span className='brand'>
+              ChatGPT
+            </span>
+          </div>
+          <button className='midButton'>
+            <img src='' alt='' className='addChatButton'/>
+            Nouveau Chat
+          </button>
+    {/*---------- upperSideBottom -------------*/}
+          <div className='upperSideBottom'>
+            {/*---------- Exemple de Question -------------*/}
+            <button className='query'><img src='' alt=''/>C'est quoi la programmation ?</button>
+            <button className='query'><img src='' alt=''/>Comment utiliser une API ?</button>
+          </div>
+
 
         </div>
+
+
+{/*---------- LowerSide -------------*/}
         <div className='lowerSide'>
 
         </div>

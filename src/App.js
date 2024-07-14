2b56b0242aef2c7eb413e00073fc052a8ad465c1
unknown <robpoquin@gmail.com>
2024-07-14 12:47:54 +0200

    Creation SideBar partie base, ajout des styles et icones

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 import gptLogo from './assets/chatgpt.svg';
+import addBtn from './assets/add-30.png';
+import messageIcon from './assets/message.svg';
+import home from './assets/home.svg';
+import saved from './assets/bookmark.svg';
+import rocket from './assets/rocket.svg';
 
 function App() {
   return (
@@ -15,23 +20,30 @@ function App() {
             </span>
           </div>
           <button className='midButton'>
-            <img src='' alt='' className='addChatButton'/>
+            <img src={addBtn} alt='new Chat' className='addChatButton'/>
             Nouveau Chat
           </button>
     {/*---------- upperSideBottom -------------*/}
           <div className='upperSideBottom'>
             {/*---------- Exemple de Question -------------*/}
-            <button className='query'><img src='' alt=''/>C'est quoi la programmation ?</button>
-            <button className='query'><img src='' alt=''/>Comment utiliser une API ?</button>
+            <button className='query'>
+              <img src={messageIcon} alt='query'/>
+              C'est quoi la programmation ?
+            </button>
+            <br/>
+            <button className='query'>
+              <img src={messageIcon} alt='query'/>
+              Comment utiliser une API ?
+            </button>
           </div>
-
-
         </div>
 
 
 {/*---------- LowerSide -------------*/}
         <div className='lowerSide'>
-
+          <div className='listItems'><img src={home} alt='' className='listItemsImg'/>Acceuil</div>
+          <div className='listItems'><img src={saved} alt='' className='listItemsImg'/>Enregistrement</div>
+          <div className='listItems'><img src={rocket} alt='' className='listItemsImg'/>Mise à Jour Plan</div>
         </div>
       </div>
       <div className='main'>
